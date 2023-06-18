045d91b9107c66613eda470507284ba43538a0d9
Mac-collins Lawal <m.collinsdeone@gmail.com>
2023-06-18 11:56:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Print out the statement in put command
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a mutilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
