110a704417053a29a74d3d920547c0107a1fabc9
Mac-collins Lawal <m.collinsdeone@gmail.com>
2023-06-18 11:32:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(void)
-	 {
-		 puts("\"Programming is like building a mutilingual puzzle");
-		 return 0;
-	 }
+{
+	puts("\"Programming is like building a mutilingual puzzle");
+	return (0);
+}
+
