758de0adedb012e24a38deae6bbc6f7c6f3cdae6
Mac-collins Lawal <m.collinsdeone@gmail.com>
2023-06-19 15:37:38 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,35 +2,30 @@
 
 /**
  * main - Entry point
- *
+ * the code prints a combination of two numbers
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int i, j, k;
+	int i, j;
 
-	for (i = 0; i <= 7; i++)
+	for (i = 0; i <= 8; i++)
 	{
-		for (j = i + 1; j <= 8; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			for (k = j + 1; k <= 9; k++)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
+			putchar(i + '0');
+			putchar(j + '0');
 
-				if (i != 7 || j != 8 || k != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
-
-		putchar('\n');
-
-		return (0);
 	}
+
+	putchar('\n');
+
+	return 0;
 }
 
