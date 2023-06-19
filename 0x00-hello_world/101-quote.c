f76cd41bc2dc59eb6f3d297dec262a1d8e8fe514
Mac-collins Lawal <m.collinsdeone@gmail.com>
2023-06-19 16:41:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,13 @@
 
 /**
  * main - Entry point
- *Prints out error message
+ * prints out text
  * Return: Always 1 (Failure)
  */
 int main(void)
-
 {
-	write(2, "and that piece of art is useful", 28);
-	write(2, "\" - Dora Korpar, 2015-10-19\n", 31);
+	write(2, "and that piece of art is useful\" - ", 36);
+	write(2, "Dora Korpar, 2015-10-19\n", 25);
 	return (1);
 }
 
