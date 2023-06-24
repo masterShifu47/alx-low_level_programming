6cd2df2845090d419b4ce771d79b66af2792138a
Mac-collins Lawal <m.collinsdeone@gmail.com>
2023-06-23 17:07:45 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,38 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Task 0: isupper */
+int _isupper(int c);
+
+/* Task 1: isdigit */
+int _isdigit(int c);
+
+/* Task 2: mul */
+int mul(int a, int b);
+
+/* Task 3: print_numbers */
+void print_numbers(void);
+
+/* Task 4: print_most_numbers */
+void print_most_numbers(void);
+
+/* Task 5: more_numbers */
+void more_numbers(void);
+
+/* Task 6: print_line */
+void print_line(int n);
+
+/* Task 7: print_diagonal */
+void print_diagonal(int n);
+
+/* Task 8: print_square */
+void print_square(int size);
+
+/* Task 9: fizz_buzz */
+void fizz_buzz(void);
+
+/* Task 10: print_triangle */
+void print_triangle(int size);
+
+#endif /* MAIN_H */
+
