7aeda154a86d0fa5f43fe763b326ed879a17b246
Mac-collins Lawal <m.collinsdeone@gmail.com>
2023-06-30 16:16:22 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13
+ * @str: The string to encode
+ *
+ * Return: The encoded string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot13_alphabet[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = rot13_alphabet[j]
+					break;
+			}
+		}
+	}
+
+	return (str);
+}
+
